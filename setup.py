39f8eb155b4e13338f743f1218bfab226bc9eb98
adgsenpai <adg@adgstudios.co.za>
2023-05-31 21:33:07 +0200

    Uploaded Module

--- setup.py
+++ setup.py
@@ -4,23 +4,27 @@ this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
 setup(
-    #metadata here
-    name="mymodule",
+    # metadata here
+    name="MicrosoftGraphAPI",
     long_description=long_description,
     long_description_content_type='text/markdown',
     version="0.0.1",
     author="Ashlin Darius Govindasamy",
     author_email="adg@adgstudios.co.za",
     url="https://www.adgstudios.co.za",
-    description="templete for module generation",
+    description="A Python package to interact with the Microsoft Graph API",
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    #license here
-    license='MIT', 
-    #modules here
-    install_requires=["numpy"]
+    # license here
+    license='MIT',
+    # modules here
+    install_requires=[
+        'requests',
+        'pandas',
+        'msal',
+    ],
 )
